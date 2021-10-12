59264aecbb94338688bce11f97f09e866a136e70
sultanbaloch <61145760+sultanbaloch@users.noreply.github.com>
2021-10-12 16:00:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function plus() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    document.getElementById("result").innerHTML = Number (num1) + Number (num2)
+}
+
+function minus() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    document.getElementById("result").innerHTML = Number (num1) - Number (num2)
+}
+
+function multiply() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    document.getElementById("result").innerHTML = Number (num1) * Number (num2)
+}
+function divide() {
+
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    document.getElementById("result").innerHTML = Number (num1) / Number (num2)
+}
